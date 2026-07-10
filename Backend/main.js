37d0084f2d3551fea4a526b65a87bf475b74f8b6
Anna Maria Simonetti <annasimo1996@gmail.com>
2026-07-10 16:43:32 +0200

    refactoring

--- Backend/main.js
+++ Backend/main.js
@@ -2,31 +2,24 @@ require("dotenv/config");
 const express = require("express");
 const cors = require("cors");
 const startDb = require("./config/db.js"); 
-const usersRouter = require("./modules/users/usersRoute.js");
-const authRouter = require("./modules/auth/authRoute.js");
+const usersRoute = require("./modules/users/usersRoute.js");
+const authRoute= require("./modules/auth/authRoute.js");
 const errorHandler = require("./middlewares/errorHandler.js");
-
+const clientsRoute = require("./modules/clients/clientsRoute.js")
 const server = express();
 const PORT = process.env.PORT;
+
+ const allowedOrigins = [
+        "http://localhost:5173",
+        "http://localhost:9998",
+        "https://flowbix.vercel.app",
+        process.env.CLIENT_URL,
+      ];
 
 server.use(express.json()); 
 server.use(
   cors({
-    origin: (origin, callback) => {
-      const allowedOrigins = [
-        "http://localhost:5173",
-        "http://localhost:9998",
-        process.env.CLIENT_URL,
-      ];
-      
-      // Permetti richieste senza origin (es. Postman o health check) o se il dominio è esplicitamente nell'array
-      // Oppure se l'origine contiene "vercel.app" (così accetta qualsiasi tua anteprima)
-      if (!origin || allowedOrigins.includes(origin) || origin.includes("vercel.app")) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: allowedOrigins,
     credentials: true,
   })
 );
@@ -37,8 +30,9 @@ server.get("/", (req, res) => {
   res.send("API di FlowBix funzionanti al 100%!");
 });
 
-server.use("/users", usersRouter);
-server.use("/auth", authRouter);
+server.use("/users", usersRoute);
+server.use("/clients", clientsRoute)
+server.use("/auth", authRoute);
 
 server.use(errorHandler);
 
